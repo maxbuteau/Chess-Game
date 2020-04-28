373b82bb208a082fafe8bd6770fa800dc7bb1798
Maxime Buteau <maxbut@live.ca>
2020-04-28 16:25:36 -0400

    Initial commit, board displays

---
+++ piece.py
@@ -0,0 +1,31 @@
+class Piece:
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+
+class Pawn(Piece):
+    pass
+
+
+class Rook(Piece):
+    pass
+
+
+class Knight(Piece):
+    pass
+
+
+class Bishop(Piece):
+    pass
+
+
+class Queen(Piece):
+    pass
+
+
+class King(Piece):
+    pass
+
+
